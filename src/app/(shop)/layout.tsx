a23cad0aed1ae6bb0b4d0a077d9f93f74129a7cc
Juan Jose Mayorga <devjuanjose@gmail.com>
2023-11-28 20:04:44 -0600

    Add react-icons package and create TopMenu component

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,9 +1,12 @@
+import { TopMenu } from "@/components";
 
 export default function ShopLayout({ children }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-red-500">
+    <div className="min-h-screen">
+      <TopMenu />
+
       {children}
     </div>
   );
