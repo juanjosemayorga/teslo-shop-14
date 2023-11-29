038a848d0e58289f010d015f4da1f5b400a9c60b
Juan Jose Mayorga <devjuanjose@gmail.com>
2023-11-28 20:18:30 -0600

    Fix category page not found issue

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,22 @@
+import { notFound } from "next/navigation";
 
-export default function CategoryPage() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+  console.log('id', id);
+
+  if (id === "kids") {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>CategoryPage</h1>
+      <h1>CategoryPage {id}</h1>
     </div>
   );
 }
