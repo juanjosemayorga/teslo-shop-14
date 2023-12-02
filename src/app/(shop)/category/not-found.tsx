9f101da355917cdd4f3d93303c921a72ca3ea390
Juan Jose Mayorga <devjuanjose@gmail.com>
2023-12-02 12:55:57 -0600

    custom 404 page created

--- src/app/(shop)/category/not-found.tsx
+++ src/app/(shop)/category/not-found.tsx
@@ -1,10 +1,8 @@
+import { PageNotFound } from "@/components";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <Link href="/">Go back to home</Link>
-    </div>
+    <PageNotFound />
   );
 }
