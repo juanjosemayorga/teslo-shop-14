f16c524e82e22907f9083da35e2b66f1034e2d9c
Juan Jose Mayorga <devjuanjose@gmail.com>
2023-12-07 12:21:10 -0600

    Refactor product grid item component to handle image hover

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,21 +1,36 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
 import { Product } from "@/interfaces";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0])
+
+  const handleMouseEnter = () => {
+    setDisplayImage(product.images[1])
+  }
+
+  const handleMouseLeave = () => {
+    setDisplayImage(product.images[0])
+  }
+
   return (
     <div className="rounded-md overflow-hidden fade-in">
       <Link href={`/product/${product.slug}`}>
         <Image
-          src={`/products/${product.images[0]}`}
+          src={`/products/${displayImage}`}
           alt={product.title}
-          className="w-full object-cover"
+          className="w-full object-cover rounded"
           width={500}
           height={500}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         />
       </Link>
 
