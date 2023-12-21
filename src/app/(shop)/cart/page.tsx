b2554bf068247409902e26ee358da76f3de50f1e
Juan Jose Mayorga <devjuanjose@gmail.com>
2023-12-20 19:47:22 -0600

    Add redirect to empty cart page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Title } from "@/components";
 import { QuantitySelector } from "@/components/product/quantity-selector/QuantitySelector";
 import { initialData } from "@/seed/seed";
+import { redirect } from "next/navigation";
 
 const productsInCart = [
   initialData.products[0],
@@ -13,6 +14,9 @@ const productsInCart = [
 ]
 
 export default function CartPage() {
+
+  // redirect('/empty');
+
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
