51b7f6d4f233adb5ad35446e65212d9eaa1a78ff
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-01 20:45:02 -0600

    Refactor pagination logic and add searchParams support

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,8 +1,15 @@
 import { getPaginatedProductsWithImages } from '@/actions';
 import { ProductGrid, Title } from '@/components'
 
-export default async function Home() {
-  const { products } = await getPaginatedProductsWithImages();
+interface Props {
+  searchParams: {
+    page?: string;
+  }
+}
+
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products } = await getPaginatedProductsWithImages({ page });
 
   return (
     <>
