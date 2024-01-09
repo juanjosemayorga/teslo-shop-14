58a52e69d504045644d512a819e52bf7867970d2
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-09 12:15:00 -0600

    Add functionality to add product to cart

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -13,6 +13,7 @@ interface Props {
 export const AddToCart = ({ product }: Props) => {
   const [size, setSize] = useState<Size | undefined>();
   const [quantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState<boolean>(false);
 
   const handleSizeChange = (size: Size) => {
     setSize(size);
@@ -23,16 +24,24 @@ export const AddToCart = ({ product }: Props) => {
   }
 
   const addToCart = () => {
+    setPosted(true);
+
     if (!size) {
-      alert('Please select a size');
       return;
     }
 
-    console.log({ size, quantity });
+    console.log({ size, quantity, product });
+    // TODO: Add to cart
   }
 
   return (
     <>
+      {posted && !size && (
+        <span className="mt-2 text-red-500 fade-in">
+          You must select a size before adding to cart*
+        </span>
+      )}
+
       {/* Size Selector */}
       <SizeSelector
         selectedSize={size}
---
+++ src/store/cart/cart-store.ts
@@ -0,0 +1,44 @@
+import { CartProduct } from "@/interfaces";
+import { create } from "zustand";
+
+interface State {
+  cart: CartProduct[];
+
+  addProductToCart: (product: CartProduct) => void;
+  // updateProductQuantity: (product: CartProduct) => void;
+  // removeProduct: (product: CartProduct) => void;
+}
+
+export const useCartStore = create<State>(
+  (set, get) => ({
+    cart: [],
+    addProductToCart: (product: CartProduct) => {
+      const { cart } = get();
+
+      // To review if the product exists with the selected size
+      const productInCart = cart.some(
+        (item) =>
+          item.id === product.id && item.size === product.size
+      )
+
+      if (!productInCart) {
+        set({ cart: [...cart, product] });
+        return;
+      }
+
+      // The product exists, so we update the quantity
+      const updatedCartProducts = cart.map((item) => {
+        if (item.id === product.id && item.size === product.size) {
+          return {
+            ...item,
+            quantity: item.quantity + product.quantity,
+          };
+        }
+
+        return item;
+      });
+
+      set({ cart: updatedCartProducts });
+    },
+  })
+);
