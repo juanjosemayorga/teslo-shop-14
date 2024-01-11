7693b9131023618f36fd412be4be91d45e098a60
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-11 12:33:57 -0600

    Add zod package and implement credentials provider in auth.config.ts

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,38 @@
-import type { NextAuthConfig } from 'next-auth';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/new-account',
   },
-  providers: []
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+          if (!parsedCredentials.success) {
+            return null;
+          }
+
+          const { email, password } = parsedCredentials.data;
+          console.log({ email, password });
+
+          // Search for the email
+
+          // Check if the password is correct
+
+          return null;
+      },
+    }),
+  ]
 }
+
+export const {
+  signIn,
+  signOut,
+  auth
+} = NextAuth(authConfig);
