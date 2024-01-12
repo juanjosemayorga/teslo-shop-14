375fd3870971bd4c7d9b260da8ac14a5860c4402
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-12 12:45:03 -0600

    Update LoginForm component and add LoginButton component

--- src/app/auth/ui/LoginForm.tsx
+++ src/app/auth/ui/LoginForm.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
 import Link from "next/link";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 import { authenticate } from "@/actions";
+import { IoInformationOutline } from "react-icons/io5";
+import clsx from "clsx";
 
 export const LoginForm = () => {
-  const [state, dispatch] = useFormState(authenticate, undefined);
-
-  console.log(state);
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
     <form action={dispatch} className="flex flex-col">
@@ -25,7 +25,20 @@ export const LoginForm = () => {
         name="password"
       />
 
-      <button type="submit" className="btn-primary">Ingresar</button>
+      <div
+        className="flex h-8 items-end space-x-1"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {errorMessage && (
+          <div className="flex flex-row mb-2">
+            <IoInformationOutline className="h-5 w-5 text-red-500" />
+            <p className="text-sm text-red-500">{errorMessage}</p>
+          </div>
+        )}
+      </div>
+
+      <LoginButton />
 
       {/* divisor l ine */}
       <div className="flex items-center my-5">
@@ -40,3 +53,20 @@ export const LoginForm = () => {
     </form>
   );
 };
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      className={clsx({
+        "btn-primary": !pending,
+        "btn-disabled": pending,
+      })}
+      disabled={pending}
+    >
+      Ingresar
+    </button>
+  );
+}
