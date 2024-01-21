cac8eb99bcc9f689d6a2405a0d1e3b2d0f69a874
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-20 18:53:52 -0600

    Refactor authorized callback in auth.config.ts

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -11,6 +11,22 @@ export const authConfig: NextAuthConfig = {
     newUser: '/auth/new-account',
   },
   callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      // const isLoggedIn = !!auth?.user;
+      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) {
+      //     return true;
+      //   }
+
+      //   return false;
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL('/dashboard', nextUrl));
+      // }
+
+      return true;
+    },
     jwt({ token, user }) {
 
       if (user) {
