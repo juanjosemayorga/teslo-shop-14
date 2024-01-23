740487465e6cad889a3f82db9a33a963431b934f
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-23 12:52:28 -0600

    Add country selection to address form

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,13 +1,16 @@
+import { getCountries } from '@/actions';
 import { Title } from '../../../../components/ui/title/Title';
 import { AddressForm } from "./ui/AddressForm";
 
-export default function AddressPage() {
+export default async function AddressPage() {
+  const countries = await getCountries();
+
   return (
     <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
       <div className="w-full  xl:w-[1000px] flex flex-col justify-center text-left">
         <Title title="Dirección" subTitle="Dirección de entrega" />
 
-        <AddressForm />
+        <AddressForm countries={countries} />
       </div>
     </div>
   );
