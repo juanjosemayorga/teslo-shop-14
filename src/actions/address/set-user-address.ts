6340776f1538b40936308ca956b844ec06c4ae07
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-24 18:20:20 -0600

    Add address functionality and export address interface

---
+++ src/actions/address/set-user-address.ts
@@ -0,0 +1,59 @@
+'use server';
+
+import { Address } from "@/interfaces";
+import prisma from "@/lib/prisma";
+
+export const setUserAddress = async(address: Address, userId: string) => {
+  try {
+    const newAddress = await createOrReplaceAddress(address, userId);
+
+    return {
+      ok: true,
+      address: newAddress
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ok: false,
+      error: 'Error setting user address'
+    }
+  }
+};
+
+const createOrReplaceAddress = async(address: Address, userId: string) => {
+  try {
+    const storedAddress = await prisma.userAddress.findUnique({
+      where: { userId }
+    });
+
+    const addressToSave = {
+      userId,
+      address: address.address,
+      address2: address.address2,
+      countryId: address.country,
+      firstName: address.firstName,
+      lastName: address.lastName,
+      phone: address.phone,
+      postalCode: address.postalCode,
+    };
+
+    if (!storedAddress) {
+      const newAddress = await prisma.userAddress.create({
+        data: addressToSave,
+      });
+
+      return newAddress;
+    }
+
+    const updatedAddress = await prisma.userAddress.update({
+      where: { userId },
+      data: addressToSave,
+    });
+
+    return updatedAddress;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error creating or replacing address');
+  }
+};
