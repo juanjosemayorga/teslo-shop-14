86d6e08e6f47d1603dca2b3dc162a47d14a8a67f
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-01-27 17:14:19 -0600

    Add product information and calculate total price in placeOrder function

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import type { Address, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
 
 interface ProductToOrder {
   productId: string;
@@ -13,6 +14,7 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
   const session = await auth();
   const userId = session?.user?.id;
 
+  // Check if there is a user logged in
   if (!userId) {
     return {
       ok: false,
@@ -20,6 +22,20 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
     }
   }
 
-  console.log({ productIds, address, userId });
+  // Get the products information
+  // Note: we can take more thant two products with the same id
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map(p => p.productId),
+      }
+    }
+  });
+
+  // Calculate the total price
+  const itemsInOrder = productIds.reduce((acc, curr) => acc + curr.quantity, 0);
+
+  // Totals of tax, subtotal and total
+  
 
 };
