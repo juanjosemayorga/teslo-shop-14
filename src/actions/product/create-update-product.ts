be7453a20db48d7533e46b366c48a264eea38746
Juan Jose Mayorga <devjuanjose@gmail.com>
2024-02-09 08:52:14 -0600

    Add Cloudinary integration for image uploading

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { Gender, Product, Size } from '@prisma/client';
 import prisma from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
+import {v2 as cloudinary} from 'cloudinary';
+
+cloudinary.config(process.env.CLOUDINARY_URL ?? '');
 
 const productSchema = z.object({
   id: z.string().uuid().optional().nullable(),
@@ -80,7 +83,8 @@ export const createUpdateProduct = async (formData: FormData) => {
 
       // Load and save images processing
       if (formData.getAll('images')) {
-        console.log(formData.getAll('images'));
+        const images = await uploadImages(formData.getAll('images') as File[]);
+        console.log({ images });
       }
 
       return {
@@ -106,3 +110,28 @@ export const createUpdateProduct = async (formData: FormData) => {
     };
   }
 };
+
+const uploadImages = async( images: File[] ) => {
+
+  try {
+    const uploadPromises = images.map( async( image) => {
+      try {
+        const buffer = await image.arrayBuffer();
+        const base64Image = Buffer.from(buffer).toString('base64');
+
+        return cloudinary.uploader.upload(`data:image/png;base64,${ base64Image }`)
+          .then( r => r.secure_url );
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    })
+
+    const uploadedImages = await Promise.all( uploadPromises );
+    return uploadedImages;
+
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
